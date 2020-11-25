1e173ff2ecb008ca1a7d55e7e1d55baceecb84c0
MartinMilan <martin.milan3@gmail.com>
2020-11-25 12:22:02 -0300

    Se cambia nombre coleccion de reservas

--- src/main/java/com/topicos/reservations/persistence/entity/Reservacion.java
+++ src/main/java/com/topicos/reservations/persistence/entity/Reservacion.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
-@Document(collection = "reservas")
+@Document(collection = "reservaciones")
 public class Reservacion {
     @Id
     private String id;
